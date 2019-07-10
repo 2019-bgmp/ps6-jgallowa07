0096bf6314bb92505c2bb04d29d1feb4595bb8f9
Jared Galloway <jaredgalloway07@gmail.com>
2019-07-10 14:14:23 -0700

    part of part 1

--- PS6.py
+++ PS6.py
@@ -7,6 +7,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-f', type=str, help='file to parse')
 args = parser.parse_args()
 
+K_MER_LENGTH = 49
+
+sum_nucleotides_from_header = 0
+num_contigs = 0
+max_contig_length = 0
+mean_contig_length = 0
+total_contig_length = 0
+contig_legths = []
+
 k_mer_lengths = []
 k_mer_coverages = []
 with open(args.f,"r") as fafp:
@@ -15,6 +24,21 @@ with open(args.f,"r") as fafp:
             matches = re.match(".+_.+_.+_(.+)_.+_(.+)",line)
             k_mer_lengths.append(int(matches.groups()[0]))
             k_mer_coverages.append(float(matches.groups()[1]))
+            contig_length = int(matches.groups()[0]) + K_MER_LENGTH - 1
+            contig_lengths.append(contig_length)
+            max_contig_length = max(max_contig_length, contig_length)
+            sum_nucleotides_from_header += contig_length
+            num_contigs += 1
+
+mean_contig_length = total_contig_length / num_contigs
+
+
+
+
+
+
+
+            
 
             
 
